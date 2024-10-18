65f7b6e3871b46f3c3b345efe7dbc1612759dc80
Erick Cantu Paz <cantupaz@yahoo.com>
2024-10-18 04:20:10 +0000

    calculate stats, clean up

--- src/ga/ga.py
+++ src/ga/ga.py
@@ -2,6 +2,8 @@
 
 from typing import Any
 import random
+import statistics
+
 
 
 class Evaluator:
@@ -59,6 +61,10 @@ class Population:
         """Evaluate every individual in the population."""
         for i in range(self.size):
             self.inds[i].evaluate(evaluator)
+
+    def convergence(self)->float:
+        """Return a metric of the convergence of the population."""
+        return 0
 
     def stats(self):
         """Return a dict with statistics about the population (min, max, avg fitness)."""
@@ -144,13 +150,13 @@ class MultiPointCrossover:
         """Crossover two individuals."""
 
         if random.random() > self.crossover_rate:
-            return a
+            return random.choice([a,b])
 
         # set up the offspring
-        result = Individual(a.length)
-        result.sex = random.randint(0,1)
-        result.appeal = random.choice([a.appeal, b.appeal])
-        result.genes = []
+        offspring = Individual(a.length)
+        offspring.sex = random.randint(0,1)
+        offspring.appeal = random.choice([a.appeal, b.appeal])
+        offspring.genes = []
 
         # Generate unique random crossover points
         crossover_points = sorted(random.sample(range(1, a.length), self.num_crossover_points))
@@ -160,21 +166,21 @@ class MultiPointCrossover:
         start = 0
         for point in crossover_points:
             if use_a:
-                result.genes.extend(a.genes[start:point])
+                offspring.genes.extend(a.genes[start:point])
             else:
-                result.genes.extend(b.genes[start:point])
+                offspring.genes.extend(b.genes[start:point])
 
             use_a = not use_a
             start = point
 
         # After the last crossover point, append the remaining part
         if use_a:
-            result.genes.extend(a.genes[start:])
+            offspring.genes.extend(a.genes[start:])
         else:
-            result.genes.extend(b.genes[start:])
+            offspring.genes.extend(b.genes[start:])
 
 
-        return result
+        return offspring
 
 
 
@@ -185,6 +191,7 @@ class Reproduction:
     def reproduce(self, input:Population, offspring: int)->Population:
         """Produce a new population with the number of offspring indicated. The input population does not
         have to be of the same size as the required offspring."""
+
         result = Population(offspring, input.length)
 
         # create a randomly shuffled list of parents
@@ -203,35 +210,32 @@ class Reproduction:
 
 class SexualReproduction(Reproduction):
 
+        # the females will pick one male to mate amongst k candidates
+        # possible variations: pick the one with more appeal
+        # or with appeal closest to the female's appeal
+
     def __init__(self, crossover, cohort_size: int, mating_criterion: str):
         super().__init__(crossover)
         self.cohort_size = cohort_size
         self.mating_criterion = mating_criterion
 
-    def pick_mate(self, input:Population, female:int, males: list[int])->int:
-        """Returns the index of the selected mate."""
-        mate = -1
-        if self.mating_criterion == "max_appeal":
-            mate = max(males, key=lambda idx: input[idx].appeal)
-        elif self.mating_criterion == "closest_appeal":
-            mate =  min(males, key=lambda idx: abs(input[idx].appeal - input[female].appeal))
-        return mate
+
 
     def reproduce(self, input:Population, offspring: int)->Population:
         """Produce a new population."""
         result = Population(offspring, input.length)
 
-        # the females will pick one male to mate amongst k candidates
-        # possible variations: pick the one with more appeal
-        # or with appeal closest to the female's appeal
         males: list[int] = [i for i, ind in enumerate(input.inds) if ind.sex==1]
         females: list[int] = [i for i, ind in enumerate(input.inds) if ind.sex==0]
 
-        # need to iterate this multiple times until we get enough offspring
-        current_offspring = 0
+        if males == [] or females == []:
+            print("we don't have two sexes")
+            return result
+
+        # need to iterate multiple times over the set of females until we get enough offspring
+        current_offspring = 0 # track how many offspring have been created so far
         for i in range(int(offspring/len(females)) + 1):
             self.mate_females(input, males, females, result, current_offspring, offspring)
-            # current_offspring += len(females) this is incremented inside the func
 
         return result
 
@@ -239,6 +243,8 @@ class SexualReproduction(Reproduction):
     def mate_females(self, input, males, females, result, current_offspring, offspring):
         # the females will pick one mate amongst k candidates. we will pick
         # k males without replacement and reshuffle the list of males if needed
+
+        k = max(self.cohort_size, len(males))
 
         # Track the current position in the male_indices list
         current_position = 0
@@ -248,22 +254,32 @@ class SexualReproduction(Reproduction):
                 break
 
             # If there aren't enough males left in the list to pick k, reshuffle the males
-            if current_position + self.cohort_size > len(males):
+            if current_position + k > len(males):
                 random.shuffle(males)
                 current_position = 0
 
             # Select k males from the current position
-            selected_males = males[current_position:current_position + self.cohort_size]
+            selected_males = males[current_position:current_position + k]
 
-            # Send the selected males and current female to the do_something function
+            # the current female picks a mate from the selected males
             mate = self.pick_mate(input, female, selected_males)
+
             result[current_offspring] = self.crossover.crossover(input[female], input[mate])
             current_offspring += 1
 
             # Move the position forward by k
-            current_position += self.cohort_size
+            current_position += k
 
-
+    def pick_mate(self, input:Population, female:int, males: list[int])->int:
+        """Returns the index of the selected mate."""
+        mate = -1
+        if self.mating_criterion == "max_appeal":
+            mate = max(males, key=lambda idx: input[idx].appeal)
+        elif self.mating_criterion == "closest_appeal":
+            mate =  min(males, key=lambda idx: abs(input[idx].appeal - input[female].appeal))
+        elif self.mating_criterion == "max_fitness":
+            mate = max(males, key=lambda idx: input[idx].fitness)
+        return mate
 
 class GeneticAlgorithm:
 
@@ -274,36 +290,73 @@ class GeneticAlgorithm:
         self.selection:Selection = config.get("selection", TournamentSelection())
         self.reproduce:Reproduction = config.get("reproduction", Reproduction(MultiPointCrossover()))
         self.evaluator: Evaluator = config.get("evaluator", TrapK())
+        self.init_GA()
 
+    def init_GA(self)->None:
+        self.stats: dict[str, float] = {}
         self.population:Population = Population(self.popsize, self.length)
 
+    def done(self)->bool:
+        return self.stats["generation"] >= 10
+
     def run(self):
-        # selected = Population(self.popsize, self.length)
-
+        self.init_GA()
         self.population.evaluate(self.evaluator)
-        print(self.population.stats())
+        self.stats.update(self.population.stats())
+        self.stats["generation"] = 0
 
-        for generation in range(0,20):
+        while (not self.done()):
             selected = self.selection.select(self.population)
             self.population = self.reproduce.reproduce(selected, self.popsize)
             self.population.evaluate(self.evaluator)
-            print(self.population.stats())
-            # self.population.print()
+            self.stats.update(self.population.stats())
+            self.stats["generation"] += 1
+            # print(self.stats)
 
 
 
+
+def calculate_metrics_stats(data: list[dict]) -> dict:
+    """Return a dict with mean and standard deviation of metrics. The input is a list
+    of dictionaries with metrics [{"max_fitness":100, ...}, {"max_fitness":98, ...}]
+    and the output is a dict with {"max_fitness":{"mean": 99 ,"std":1.5}}"""
+
+    metrics_stats = {}
+
+    # For each key (metric), calculate the mean and standard deviation
+    for key in data[0].keys():
+        values = [d[key] for d in data]
+        mean_value = statistics.mean(values)
+        stddev_value = statistics.stdev(values) if len(values) > 1 else 0.0
+        metrics_stats[key] = {'mean': mean_value, 'stddev': stddev_value}
+
+    return metrics_stats
+
+def print_metrics_stats(stats):
+    for key in stats:
+        print(f"{key:>20}: {stats[key]['mean']:>8.2f} {stats[key]['stddev']:>8.2f}")
 
 
 def main():
-    config = {}
-    config["popsize"] = 20
-    config["length"] = 20
-    config["evaluator"] = OneMax() #TrapK(k=4)
-    config["selection"] = TruncationSelection(0.5) #TournamentSelection(k=2)
-    config["reproduction"] = Reproduction(MultiPointCrossover(num_crossover_points=2, crossover_rate=1.0))
 
+    crossover = MultiPointCrossover(num_crossover_points=2, crossover_rate=1.0)
+    config = {}
+    config["popsize"] = 100
+    config["length"] = 80
+    config["evaluator"] = OneMax() #TrapK(k=4)
+    config["selection"] = TournamentSelection(k=2) #TruncationSelection(0.5)
+    config["reproduction"] = SexualReproduction(crossover, 5, "closest_appeal") #Reproduction(crossover)
+    config["reproduction"] = Reproduction(crossover)
     ga = GeneticAlgorithm(config)
-    ga.run()
+
+    # run the GA multiple times and collect metrics
+    stats = []
+    for i in range(0,10):
+        ga.run()
+        stats.append(ga.stats)
+        # print(ga.stats)
+
+    print_metrics_stats(calculate_metrics_stats(stats))
 
 if __name__ == '__main__':
     main()
